9b6f9e6f747a90d1e82bb306fdca65a33c54bab0
MrPeaceDuke <daemonica120@yandex.ru>
2020-03-13 18:34:26 +0500

    Server + streaming

--- server.js
+++ server.js
@@ -8,9 +8,24 @@ server.listen(80);
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
+var capturedStream = null;
 io.on('connection', function (socket) {
-  socket.on('signal', function (data) {
+    
+    socket.on('signal', function (data) {
+        socket.emit('signal', capturedStream);
+    });
+    socket.on('test', function (data) {
+        console.log(data);
+    });
+    socket.on('streaming', function (data) {
+        console.log('Streaming running');
+        capturedStream = data;
+    });
+});
+io.on('steaming', function (data) {
+    capturedStream = data;
     console.log(data);
-  });
+});
+io.on('signal', function (data) {
+    socket.emit('signal', capturedStream);
 });
