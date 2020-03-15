f573f58db6038cd1fe071365def6d1ddc3ef1eaa
Litand <daemonica120@yandex.ru>
2020-03-15 18:58:55 +0500

    heroku

--- src/view.js
+++ src/view.js
@@ -1,22 +1,51 @@
 const io = require('socket.io-client');
 const $ = require('jquery');
 
-var canvas = document.getElementById('viewer');
-
 var socket = io.connect();
 
-var blobs = [];
-var concatenatedBlobs = new Blob(blobs);
+
+
+
+var arrayOfBlobs = [];
+setInterval(function() {
+	arrayOfBlobs.append(nextChunk());
+	appendToSourceBuffer();
+}, 1000);
+
+var mediaSource = new MediaSource();
+
+var url = URL.createObjectURL(mediaSource);
+
+var video = document.getElementById("viewer");
+video.src = url;
+
+var sourceBuffer = null;
+mediaSource.addEventListener("sourceopen", function()
+{
+	sourceBuffer = mediaSource.addSourceBuffer("video/webm; codecs=\"opus,vp8\"");
+	sourceBuffer.addEventListener("updateend", appendToSourceBuffer);
+});
+function appendToSourceBuffer()
+{
+	if (
+		mediaSource.readyState === "open" &&
+		sourceBuffer &&
+		sourceBuffer.updating === false
+	)
+	{
+		sourceBuffer.appendBuffer(arrayOfBlobs.shift());
+	}
+
+	if (
+		video.buffered.length &&
+		video.buffered.end(0) - video.buffered.start(0) > 1200
+	)
+	{
+		sourceBuffer.remove(0, video.buffered.end(0) - 1200)
+	}
+}
 
 socket.on('signal', (videoStream) => {
 	console.log(videoStream);
-	canvas.src = videoStream;
-	
-	canvas.load();
-	canvas.play();
-	// canvas.onloadeddata = function() {
-	// 	URL.revokeObjectURL(canvas.srcObject);
-		
-	// }
-	
+	sourceBuffer.appendBuffer(videoStream);
 });
