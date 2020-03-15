5f3c94bde14cb521b707402a2a63ded9f920dc91
Litand <daemonica120@yandex.ru>
2020-03-15 19:26:12 +0500

    heroku

--- src/view.js
+++ src/view.js
@@ -6,20 +6,24 @@ var socket = io.connect();
 var check = true;
 var videoData = [];
 var superBuffer = null;
-
+let blobArray = [];
 var check = true;
 socket.on('signal', (videoStream) => {
-	var video = document.querySelector('viewer');
-	var videoUrl = window.URL.createObjectURL(videoStream.data);
-	video.src = videoUrl;
-	video.load();
-	video.onloadeddata = function() {
-		URL.revokeObjectURL(video.src);
-		video.play();
-	}
-	//video.srcObject
+	var video = document.getElementById('viewer');
+	// var videoUrl = window.URL.createObjectURL(videoStream.data);
+	// video.src = videoUrl;
+	// video.load();
+	// video.onloadeddata = function() {
+	// 	URL.revokeObjectURL(video.src);
+	// 	video.play();
+	// }
 
-	//video.play();
+	blobArray.push(new Blob([new Uint8Array(videoStream)],{'type':'video/mp4'}));
+	let currentTime = video.currentTime;
+	let blob = new Blob(blobArray,{'type':'video/mp4'});
+	video.src = window.URL.createObjectURL(blob);
+	video.currentTime = currentTime;
+	video.play();
 
 	console.table(videoStream);
 });
